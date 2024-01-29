89bccb897b6964bc000246e0a57f5204a210f85f
Crowdbotics <team@crowdbotics.com>
2024-01-29 21:33:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fleetcor-assetguard-45678",
+  url: "https://fleetcor-assetguard-45678.botics.co",
+  api: "https://fleetcor-assetguard-45678.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
